2b22a48d42362627cf7ca706544530f35284b620
Rotem Brillant <halorotem@gmail.com>
2024-10-17 20:44:39 +0300

    saved data to json format instead of csv

--- main.py
+++ main.py
@@ -5,6 +5,8 @@
 # English-French https://en.wiktionary.org/wiki/Appendix:False_friends_between_English_and_French
 
 from transformers import pipeline
+import json
+from transformers import AutoTokenizer, AutoModelForCausalLM
 import datasets as ds
 import pandas as pd
 from IPython.display import display
@@ -13,78 +15,75 @@ from tokenizers.models import BPE, Unigram
 from tokenizers.trainers import BpeTrainer, UnigramTrainer
 from tokenizers.pre_tokenizers import Whitespace
 
-
 def get_dataset(ds_name, split, config=None):
-  """
-  Loads the HuggingFace dataset
-  :param ds_name: the name of the dataset in huggingface format
-  :param split: the chosen split (train, validation or test)
-  :param config: the chosen configuration
-  :return: the dataset
-  """
-  if config is None:
-    return ds.load_dataset(ds_name, split=split)
-  else:
-    return ds.load_dataset(ds_name, config, split=split)
-
-def ds_to_csv(data, file_name):
-  """
-  Saves the Dataset to csv format
-  :param data: the dataset
-  :param file_name: the file name
-  :return:
-  """
-  data.to_csv(file_name, index=False)
+    """
+    Loads the HuggingFace dataset
+    :param ds_name: the name of the dataset in huggingface format
+    :param split: the chosen split (train, validation or test)
+    :param config: the chosen configuration
+    :return: the dataset
+    """
+    if config is None:
+        return ds.load_dataset(ds_name, split=split)
+    else:
+        return ds.load_dataset(ds_name, config, split=split)
 
 
-def lower_case_data(df):
-  """
-  Lower cases all the data in the dataset
-  :param df: the pandas dataframe
-  :return: the lowercased dataframe
-  """
-  for column in df.columns:
-    df[column] = df[column].str.lower()
-  return df
+def ds_to_json(data, file_name):
+    """
+    Saves the Dataset to csv format
+    :param data: the dataset
+    :param file_name: the file name
+    :return:
+    """
+    data.to_json(file_name)
 
+
+def lower_case_data(dataset):
+    """
+    Lower cases all the data in the dataset
+    :param dataset: the huggingface dataset
+    :return: the lowercased dataset
+    """
+    for key in dataset:
+        dataset[key] = dataset[key].lower()
+    return dataset
+
+def ff_filter(row, col1, col2):
+    return row[col1] == row[col2]
+    
+def generate_sentence(word, language, generator, sentence_length=100):
+    prompt = f"Write a sentence in {language} using the word '{word}'"
+    # response is a list of dictionaries
+    response = generator(text_inputs=prompt, max_length=sentence_length)
+    return response
 
 
 if __name__ == '__main__':
+    
+    unk_token = "<UNK>"  # token for unknown words
+    spl_tokens = ["<UNK>", "<SEP>", "<MASK>", "<CLS>"]  # special tokens
+    preprocess_and_dl = True
+    # Loading and Saving the different datasets: english dataset, german dataset, false friends english-german
+    # Preprocessing on the datasets
+    if preprocess_and_dl:
+        row_limit = 1000000
+        split = f"train[:{row_limit}]"
+        
+        en_ds = get_dataset("Salesforce/wikitext", split, "wikitext-103-raw-v1")
+        en_ds = en_ds.map(lower_case_data)
+        ds_to_json(en_ds, "en_ds_train.json")
 
-  unk_token = "<UNK>"  # token for unknown words
-  spl_tokens = ["<UNK>", "<SEP>", "<MASK>", "<CLS>"]  # special tokens
-  download = False
-  # Loading and Saving the different datasets: english dataset, german dataset, false friends english-german
-  if download:
-    row_limit = 1000000
-    split = f"train[:{row_limit}]"
+        ger_ds = get_dataset("stefan-it/german-dbmdz-bert-corpus", split)
+        ger_ds = ger_ds.map(lower_case_data)
+        ds_to_json(ger_ds, "ger_ds_train.json")
 
-    en_ds = get_dataset("Salesforce/wikitext", split, "wikitext-103-raw-v1")
-    ds_to_csv(en_ds, "en_ds_train.csv")
-
-    ger_ds = get_dataset("stefan-it/german-dbmdz-bert-corpus", split)
-    ds_to_csv(ger_ds, "ger_ds_train.csv")
-
-    ff_en_ger = get_dataset("aari1995/false_friends_en_de", split)
-    ds_to_csv(ff_en_ger, "ff_en_ger_train.csv")
-
-  # Loading files to pandas
-  en_ds = pd.read_csv("en_ds_train.csv")
-  ger_ds = pd.read_csv("ger_ds_train.csv")
-  ff_en_ger = pd.read_csv("ff_en_ger_train.csv")
-
-  # Taking only subset of FF df
-  cols = ["False Friend", "Correct English Translation", "Wrong English Translation", "Sentence"]
-  ff_en_ger = ff_en_ger[cols]
-
-  # Lower casing all datasets
-  en_ds = lower_case_data(en_ds)
-  ger_ds = lower_case_data(ger_ds)
-  ff_en_ger = lower_case_data(ff_en_ger)
-
-  # False Friends dataset pre-processing: filter out words that are not written exactly the same
-  mask = ff_en_ger["False Friend"] != ff_en_ger["Wrong English Translation"]
-  ff_eng_ger = ff_en_ger[~mask].reset_index(drop=True)
-  display(ff_eng_ger)
-
+        ff_en_ger = get_dataset("aari1995/false_friends_en_de", split)
+        ff_en_ger = ff_en_ger.map(lower_case_data)
+        ff_en_ger = ff_en_ger.rename_column("Sentence", "Ger Sentence")
+        # Taking only subset of FF and filter out words that are not written exactly the same
+        cols = ["False Friend", "Correct English Translation", "Wrong English Translation", "Ger Sentence"]
+        ff_en_ger = ff_en_ger.select_columns(cols)
+        ff_en_ger = ff_en_ger.filter(lambda row: ff_filter(row, "False Friend", "Wrong English Translation"))
+        ds_to_json(ff_en_ger, "ff_en_ger_train.json")
 
